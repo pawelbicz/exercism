d313de1c934ec5f036e57173856b704cf723c21a
Pawel Bicz <pbiczu@gmail.com>
2019-01-23 22:07:59 +0100

    is_triangle () function added

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,4 +1,6 @@
 def is_equilateral(sides):
+    if is_triangle(sides) is not True:
+        return False
     result = list(filter(lambda x: x == sides[0], sides))
     if len(result) == 3:
         return True
@@ -11,3 +13,10 @@ def is_isosceles(sides):
 
 def is_scalene(sides):
     pass
+
+
+def is_triangle(s):
+    r = list(filter(lambda x: x >0 , s))
+    if len(r) == 3:
+        return True
+    return False

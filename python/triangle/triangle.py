6964e40915b6bec9afb59c9873a5ee85a09745bb
Pawel Bicz <pbiczu@gmail.com>
2019-02-03 16:22:01 +0100

    function comments added

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,22 +1,26 @@
 def is_equilateral(sides):
+    """ Return True if triangle is equilateral, otherwise return False """
     if len(list(set(sides))) == 1 and is_triangle(sides):
         return True
     return False
 
 
 def is_isosceles(sides):
+    """ Return True if triangle is isosceles, otherwise return False """
     if len(list(set(sides))) < 3 and is_triangle(sides):
         return True
     return False
 
 
 def is_scalene(sides):
+    """ Return True if triangle is scalene, otherwise return False """
     if len(list(set(sides))) == 3 and is_triangle(sides):
         return True
     return False
 
 
 def is_triangle(s):
+    """ Return True if from sides 's' can be created triangle, otherwise return False """
     r = list(filter(lambda x: x > 0, s))
     if len(r) == 3 and is_properly_length_of_sides(s):
         return True
@@ -24,6 +28,7 @@ def is_triangle(s):
 
 
 def is_properly_length_of_sides(s):
+    """ Return True if length of triangle sides 's' have correct value, otherwise return False """
     longest_side = max(s)
     s.remove(longest_side)
     if sum(s) > longest_side:

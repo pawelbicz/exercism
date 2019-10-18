e064f8609d01050143ff81f3d61bcafb269ccd32
Pawel Bicz <pbiczu@gmail.com>
2019-10-18 20:23:41 +0200

    add matrix module, contain final solution

---
+++ python/matrix/matrix.py
@@ -0,0 +1,11 @@
+class Matrix(object):
+    def __init__(self, matrix_string):
+        self.matrix = [[int(i) for i in elem.split()]for elem in matrix_string.split('\n')]
+        print(self.matrix)
+
+    def row(self, index):
+        return self.matrix[index-1]
+
+    def column(self, index):
+        return [i[index-1] for i in self.matrix]
+

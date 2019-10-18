ce3779a9f3bacf56e90ca5bf296c9a8b4b83c77e
Pawel Bicz <pbiczu@gmail.com>
2019-10-18 17:35:24 +0200

    return function in convert() imporove

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,4 +1,4 @@
 def convert(number):
     raindrops = ((3, 'Pling'), (5, 'Plang'), (7, 'Plong'))
     sound = [s for n, s in raindrops if number % n == 0]
-    return ''.join(sound) if sound else str(number)
+    return ''.join(sound) or str(number)

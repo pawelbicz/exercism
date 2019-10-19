5b3d57d1ad5f6671132c7c88b3a3ce3e8502a7b6
Pawel Bicz <pbiczu@gmail.com>
2019-10-19 23:08:41 +0200

    series module, return funcion update

--- python/series/series.py
+++ python/series/series.py
@@ -2,4 +2,4 @@ def slices(series, length):
     if len(series) < length or length < 1:
         raise ValueError('Length value incorrect')
     num_of_slices = len(series) - (length-1)
-    return[series[0+i:length+i] for i in range(num_of_slices)]
+    return[series[i:length+i] for i in range(num_of_slices)]

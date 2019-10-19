d5372ed4f2f09a563d5d747ebf14e3d7a64ab018
Pawel Bicz <pbiczu@gmail.com>
2019-10-19 22:47:55 +0200

    series module add solution

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,7 @@
 def slices(series, length):
-    pass
+    if len(series) < length:
+        raise ValueError('Length of series to small')
+    if length < 1:
+        raise ValueError("Length can't be smaller than 1")
+    num_of_slices = len(series) - (length - 1)
+    return[series[0+i:length+i] for i in range(num_of_slices)]

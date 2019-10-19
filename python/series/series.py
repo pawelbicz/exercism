d9fe899c2f3bf6433e0242863ee64af7b593b264
Pawel Bicz <pbiczu@gmail.com>
2019-10-19 22:49:58 +0200

    series module, value error update

--- python/series/series.py
+++ python/series/series.py
@@ -1,7 +1,5 @@
 def slices(series, length):
-    if len(series) < length:
-        raise ValueError('Length of series to small')
-    if length < 1:
-        raise ValueError("Length can't be smaller than 1")
-    num_of_slices = len(series) - (length - 1)
+    if len(series) < length or length < 1:
+        raise ValueError('Length value incorrect')
+    num_of_slices = len(series) - (length-1)
     return[series[0+i:length+i] for i in range(num_of_slices)]

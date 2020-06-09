91cfbefad00481692b6a08da3bd39b1d30f4dd12
Pawel Bicz <pbiczu@gmail.com>
2020-06-09 22:37:12 +0200

    solution for module word-coung

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,13 @@
+import re
+
 def count_words(sentence):
-    pass
+    words = {}
+    sen = sentence.lower()
+    for word in re.split(r"\W|_", sen,):
+        if word not in words:
+           words[word] = 1
+        elif word in words:
+            words[word] += 1
+    if '' in words:
+        del words['']
+    return words

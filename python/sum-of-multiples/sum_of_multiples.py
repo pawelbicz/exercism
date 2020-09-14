3cb273f10a6712787c64d7ecde0a18e0dae6eda4
Pawel Bicz <pbiczu@gmail.com>
2020-09-14 09:52:17 +0200

    solution using list comprehension

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,6 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    return sum(set(x
+                   for multiple in multiples if multiple > 0
+                   for x in range(multiple, limit, multiple)
+                   ))
+
